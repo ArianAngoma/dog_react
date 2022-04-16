80ec3720df425822ccb409d3cf9b35fb5c671908
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-04-16 12:35:22 -0500

    State of dog images

--- src/actions/dog.ts
+++ src/actions/dog.ts
@@ -2,6 +2,7 @@ import {ThunkDispatch} from 'redux-thunk';
 import {AnyAction} from 'redux';
 
 import {fetchImageDog} from '../helpers/fetch';
+import {types} from '../types/types';
 
 export const imageDogsLoad = (breeds: string[]) => {
   return async (dispatch: ThunkDispatch<{}, {}, AnyAction>) => {
@@ -10,9 +11,18 @@ export const imageDogsLoad = (breeds: string[]) => {
           breeds.map((breed: string) => fetchImageDog(breed)),
       );
       const data = await Promise.all(resp.map((resp: any) => resp.json()));
-      console.log(data);
+      dispatch(dogSetImage(
+          data.map((item: any) => item.message)
+              .flat()),
+      );
     } catch (e) {
       console.log(e);
     }
   };
 };
+
+
+export const dogSetImage = (images: string[]) => ({
+  type: types.dogSetImage,
+  payload: images,
+});
